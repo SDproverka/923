8fad2e3793ef33f4e1a2ced0c71fab84763ce8bc
SDproverka <hateallrules@gmail.com>
2026-06-16 09:26:56 +0300

    Практическая работа 1

---
+++ r1/Задание 1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Старт проекта</p>
+    <p>Добавлен новый абзац с текстом</p>
+    <!-- Здесь могут быть последующие изменения, добавленные в шаге 12 -->
+</body>
+</html>
