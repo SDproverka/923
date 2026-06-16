1dd42880f21904adfc0a501d310b2376ae57c4c1
SDproverka <hateallrules@gmail.com>
2026-06-16 08:11:38 +0300

    Практическая работа 12

---
+++ r12/Задание_1/sample/index.php
@@ -0,0 +1,12 @@
+<?php
+// 1. Создаём переменную, содержащую текст файла
+$text = file_get_contents('text.txt');
+
+// 2. Заменяем символы переноса строк на HTML-тег <br>
+$text = preg_replace('/\r?\n/', '<br>', $text);
+
+// 3. Заменяем символы "#" на " - "
+$text = str_replace('#', ' - ', $text); // или через preg_replace: $text = preg_replace('/#/', ' - ', $text);
+
+echo $text;
+?>
